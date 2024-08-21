deb16cf04d96555cc6b928896cf086a83efa81a9
Camila Jimenez <duquejimenezcm@gmail.com>
2024-08-21 15:54:22 -0500

    Third Repository Update

--- database/config.js
+++ database/config.js
@@ -1,7 +1,17 @@
 require('dotenv')
-const moongose = require('mongoose')
+const mongoose = require('mongoose')
 
-const dbConnect = async () => {
+const dbUri = process.env.MONGO_CNN || 'mongodb+srv://jimenezcamila137:hef31bys90J*@cluster0.ezdkolb.mongodb.net/SoftSolutionsTest?retryWrites=true&w=majority&appName=Cluster0'
+
+function dbConnect() {
+    mongoose.connect(dbUri, { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true 
+    })
+    .then(() => console.log('Conectado a la base de datos'))
+    .catch(err => console.error('Error al conectar a la base de datos:', err));
+}
+/* const dbConnect = async () => {
     try {
         // connect to database
         await moongose.connect(process.env.MONGO_CNN)
@@ -9,6 +19,6 @@ const dbConnect = async () => {
     } catch (error) {
         console.log(error);
     }
-}
+} */
 
 module.exports = dbConnect  //Export the function
